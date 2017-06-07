00f6a31649d8617e1a3296dbf5cac01dad041e6b
D Dressler <eeu436@bangor.ac.uk>
2017-06-07 17:01:05 +0100

    first commit

--- doc/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['destroy',['destroy',['../class_image.html#aeb8c6219abdc8dbfdbd746bdc99149bc',1,'Image']]]
-];
--- doc/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['getmaxvalue',['getMaxValue',['../class_image.html#aca845b02170a1f79dde75162095f1c5e',1,'Image']]]
-];
